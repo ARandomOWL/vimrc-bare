c42590a4fc20ca6dec7ab400511cc0b68902067e
Adrian.Wheeldon <naw170@eeehandel.ncl.ac.uk>
2019-10-16 09:52:54 +0100

    add install.sh to fetch plugins
    Currently fetches vim-commentary and vim-surround directly from Github.

---
+++ install.sh
@@ -0,0 +1,4 @@
+cp -i ./bare.vimrc ~/.vimrc
+mkdir -p ~/.vim/plugin
+curl -o ~/.vim/plugin/commentary.vim https://raw.githubusercontent.com/tpope/vim-commentary/master/plugin/commentary.vim
+curl -o ~/.vim/plugin/surround.vim https://raw.githubusercontent.com/tpope/vim-surround/master/plugin/surround.vim
